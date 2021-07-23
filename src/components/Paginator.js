450a6f6d3ae5ca07b8925d25bb0ba946db756491
Billo Diallo <billodiallonet@gmail.com>
2021-07-23 09:13:15 +0300

    Add Pagination component

---
+++ src/components/Paginator.js
@@ -0,0 +1,39 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+
+import React from 'react';
+import { PropTypes } from 'prop-types';
+import style from '../styles/Paginator.module.css';
+
+const Paginator = ({
+  handlePaginator,
+  page,
+}) => (
+  <div className={style.paginator}>
+    <a
+      role="button"
+      href="#"
+      className="book-option middle-option"
+      onClick={() => handlePaginator('PREV')}
+    >
+      <PagePrev className={style.pageChange} />
+    </a>
+    <p>
+      {`Page ${page}/40`}
+    </p>
+    <a
+      role="button"
+      href="#"
+      className="book-option middle-option"
+      onClick={() => handlePaginator('NEXT')}
+    >
+      <PageNext className={style.pageChange} />
+    </a>
+  </div>
+);
+
+Paginator.propTypes = {
+  handlePaginator: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+};
+
+export default Paginator;
