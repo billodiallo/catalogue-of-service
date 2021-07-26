9900d6634b649d4436d3e8811bbe7cafda48392c
Billo Diallo <billodiallonet@gmail.com>
2021-07-26 13:31:44 +0300

    Add test fetch Asset list update

--- src/test/cryptoApi.test.js
+++ src/test/cryptoApi.test.js
@@ -3,3 +3,18 @@ import cryptoApi from '../reducers/cryptoApi';
 import { FETCHING_API_ASSETS, FETCHING_API_SUCCESS, FETCHING_API_FAILURE } from '../helpers/help';
 import { fetchApiAssets, fetchApiSuccess, fetchApiFailure } from '../actions/index';
 
+it('fetch API Asset list update', () => {
+  const stateBefore = [];
+  const stateAfter = {
+    fetching: true,
+    updated: false,
+    apiError: false,
+    status: FETCHING_API_ASSETS,
+  };
+  deepFreeze(stateBefore);
+  deepFreeze(fetchApiAssets);
+
+  expect(
+    cryptoApi(stateBefore, fetchApiAssets()),
+  ).toEqual(stateAfter);
+});
