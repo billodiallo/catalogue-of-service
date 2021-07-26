6ab8ed8bba9cfa26b7912bbc5cecf846ddbf5d8e
Billo Diallo <billodiallonet@gmail.com>
2021-07-26 12:43:56 +0300

    Add test update currency to new filtered currency

--- src/test/currencyFilter.test.js
+++ src/test/currencyFilter.test.js
@@ -1,3 +1,16 @@
 import deepFreeze from 'deep-freeze';
 import currencyFilter from '../reducers/currencyFilter';
 import { currencyUpdate, getCurrenCurrency } from '../actions/index';
+
+it('update currency to new filtered currency', () => {
+  const stateBefore = '';
+  const filter = 'eur';
+  const stateAfter = 'eur';
+
+  deepFreeze(stateBefore);
+  deepFreeze(currencyUpdate);
+
+  expect(
+    currencyFilter(stateBefore, currencyUpdate(filter)),
+  ).toEqual(stateAfter);
+});
