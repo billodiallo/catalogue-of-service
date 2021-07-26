32ad89e3e06ad0b7458e7fac3b847e94f25ce0e9
Billo Diallo <billodiallonet@gmail.com>
2021-07-26 11:43:35 +0300

    Add pagination test,increase page number, not going above 40

--- src/test/page.test.js
+++ src/test/page.test.js
@@ -13,3 +13,11 @@ it('increases page number, happy path', () => {
     page(stateBefore, nextPage()),
   ).toEqual(stateAfter);
 });
+
+it('test not going above 40, ', () => {
+  const stateBefore = 40;
+  const stateAfter = 40;
+  deepFreeze(stateBefore);
+  deepFreeze(stateAfter);
+  expect(page(stateBefore, nextPage())).toEqual(stateAfter);
+});
